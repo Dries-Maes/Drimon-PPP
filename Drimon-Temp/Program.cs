0d80727bf8696d916db2aed38249ed3a9cda8a62
Dries Maes <dm.inbox@outlook.com>
2021-01-07 05:45:00 +0100

    general layout

--- Drimon-Temp/Program.cs
+++ Drimon-Temp/Program.cs
@@ -1,16 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 
-
 namespace Drimon_Temp
 {
     internal class Program
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello Simon!");
-            dtest1.TestVeld();
-            Console.ReadLine();
+            MenuKlant
+
+            
         }
     }
 }
